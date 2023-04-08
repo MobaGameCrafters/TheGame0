b18f67c6c70311e375b4ff3c650677ef1368c7b1
PetrHer <petrherynek@seznam.cz>
2023-04-08 21:22:35 +0200

    collision is still a bit shitty, but ..

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -8,10 +8,11 @@ public class PlayerController : NetworkBehaviour
     private Camera  _mainCamera;
     private Vector3 _mouseInput = Vector3.zero;
     private bool isRunning;
-    private float totalDistance; // new variable to store the total distance to run
-    private float startTime; // new variable to store the start time of the run
-    private Vector3 startPosition;
+    private float totalDistance; 
+    private float startTime; 
+    private Vector2 startPosition;
     private Vector3 endPosition;
+    Rigidbody rb;
 
     private void Initialize() {
         _mainCamera = Camera.main;
@@ -20,38 +21,42 @@ public class PlayerController : NetworkBehaviour
     public override void OnNetworkSpawn(){
         Initialize();
     }
-     private void Update() {
-          if (!IsOwner || !Application.isFocused) return;
+    private void Update() {
+        rb = GetComponent<Rigidbody>();
+        if (!IsOwner || !Application.isFocused) return;
         //Movement
         if (Input.GetMouseButtonDown(1))
         {
             _mouseInput.x = Input.mousePosition.x;
             _mouseInput.y = Input.mousePosition.y;
-            _mouseInput.z = _mainCamera.nearClipPlane;
             endPosition = _mainCamera.ScreenToWorldPoint(_mouseInput);
             isRunning = endPosition != transform.position;
-            totalDistance = Vector3.Distance(transform.position, endPosition);
+            totalDistance = Vector2.Distance(transform.position, endPosition);
             startTime = Time.time;
             startPosition = transform.position;
- 
-        }
-            if (isRunning)
+
+        } }
+    private void FixedUpdate()
+    {
+        if (isRunning)
             {
                 // calculate the time it should take to run the total distance
                 float journeyLength = totalDistance;
                 float distCovered = (Time.time - startTime) * speed;
                 float fracJourney = distCovered / journeyLength;
-
-
-               transform.position = Vector3.Lerp(startPosition, endPosition, fracJourney);
- 
-                // Rotate
-                if (endPosition != transform.position)
+            Vector2 newPosition = Vector3.Lerp(startPosition, endPosition, fracJourney);
+            rb.MovePosition(newPosition);
+            // Rotate
+            if (endPosition != transform.position)
             {
                 Vector3 targetDirection = endPosition - transform.position;
-                targetDirection.z = 0f;
-                targetDirection.y = 0f;
-                transform.forward = Vector3.Slerp(transform.forward, targetDirection, Time.deltaTime * 5);
+                float angle = Mathf.Atan2(targetDirection.x, targetDirection.y) * Mathf.Rad2Deg;
+                Quaternion targetRotation = Quaternion.Euler(new Vector3(0, angle, 0));
+
+                rb.freezeRotation = true;
+                rb.MoveRotation(targetRotation);
+                rb.freezeRotation = false;
+
             }
             // check if we've reached the target position
             if (fracJourney >= 1f)
